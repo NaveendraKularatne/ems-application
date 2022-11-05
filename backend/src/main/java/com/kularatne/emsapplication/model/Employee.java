db5b1d7edccd126148e9bbad6cbb180549da21fe
DESKTOP-DGJS3IN\navee <naveendrarashmika@gmail.com>
2022-11-05 14:06:25 +0530

    Create JPA Entity Employee

---
+++ backend/src/main/java/com/kularatne/emsapplication/model/Employee.java
@@ -0,0 +1,28 @@
+package com.kularatne.emsapplication.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_id")
+    private String emailId;
+}

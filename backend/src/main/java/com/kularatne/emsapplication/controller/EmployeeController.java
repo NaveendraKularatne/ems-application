9c8d9087be978f1edda85a4c1583ad83a7687438
DESKTOP-DGJS3IN\navee <naveendrarashmika@gmail.com>
2022-11-05 14:55:25 +0530

    Update Get all employee list repository

--- backend/src/main/java/com/kularatne/emsapplication/controller/EmployeeController.java
+++ backend/src/main/java/com/kularatne/emsapplication/controller/EmployeeController.java
@@ -3,19 +3,21 @@ package com.kularatne.emsapplication.controller;
 import com.kularatne.emsapplication.model.Employee;
 import com.kularatne.emsapplication.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/v1")
 public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
 
     //Get all employees REST API
+    @GetMapping("/employees")
     public List<Employee> getAllEmployeeList() {
         return employeeService.getAllEmployees();
     }

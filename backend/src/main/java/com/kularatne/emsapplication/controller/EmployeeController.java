9e3aacdeb272e69fcbb8ff21e759aa27265747b6
DESKTOP-DGJS3IN\navee <naveendrarashmika@gmail.com>
2022-11-05 16:11:29 +0530

    Create createEmployee and getEmployeeById REST APIs

--- backend/src/main/java/com/kularatne/emsapplication/controller/EmployeeController.java
+++ backend/src/main/java/com/kularatne/emsapplication/controller/EmployeeController.java
@@ -3,10 +3,10 @@ package com.kularatne.emsapplication.controller;
 import com.kularatne.emsapplication.model.Employee;
 import com.kularatne.emsapplication.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.rmi.StubNotFoundException;
 import java.util.List;
 
 @RestController
@@ -20,5 +20,17 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<Employee> getAllEmployeeList() {
         return employeeService.getAllEmployees();
+    }
+
+    //Save/Create Employee REST API
+    @PostMapping("/create")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeService.saveEmployee(employee);
+    }
+
+    //Get an Employee by ID
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") Long id) {
+        Employee foundEmployee = employeeService.getEmployeeById(id);
+        return ResponseEntity.ok(foundEmployee);
     }
 }
--- backend/src/main/java/com/kularatne/emsapplication/service/EmployeeService.java
+++ backend/src/main/java/com/kularatne/emsapplication/service/EmployeeService.java
@@ -1,9 +1,11 @@
 package com.kularatne.emsapplication.service;
 
+import com.kularatne.emsapplication.exception.ResourceNotFoundException;
 import com.kularatne.emsapplication.model.Employee;
 import com.kularatne.emsapplication.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -16,5 +18,17 @@ public class EmployeeService {
     //Get all employee details
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
+    }
+
+    //Save an Employee
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    //Get an employee by Id
+    public Employee getEmployeeById(Long id) {
+        Employee foundEmployee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found with the id: " + id));
+        return foundEmployee;
     }
 }
